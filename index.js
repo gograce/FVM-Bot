75ed6b470c86587139dddfc09d2444e753f5e17a
Anon Assassin <testmail1878@gmail.com>
2023-02-04 14:58:08 +0530

    Trying to make a server

--- index.js
+++ index.js
@@ -10,85 +10,107 @@ const {
 const { botAddr } = require("./config");
 const { decryptConversation } = require("./decrypt");
 
-let pvtKey = "",
-	chatUser = {};
-(async function () {
-	pvtKey = await getPgpKey();
-	chatUser = await PushAPI.user.get({
-		account: botAddr,
-		env: "prod",
-	});
-})();
+const express = require("express");
 
-const main = () => {
-	try {
-		const socket = io("https://backend.epns.io", {
-			query: {
-				did: `eip155:${botAddr}`,
-				mode: "chat",
-			},
-			autoConnect: true,
-			transports: ["websocket", "polling"],
-			reconnectionAttempts: 5,
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+	res.send("Express on Vercel");
+});
+
+// Initialize server
+app.listen(5000, () => {
+	console.log("Running on port 5000.");
+	let pvtKey = "",
+		chatUser = {};
+	(async function () {
+		pvtKey = await getPgpKey();
+		chatUser = await PushAPI.user.get({
+			account: botAddr,
+			env: "prod",
 		});
+	})();
 
-		socket.on("connect", async () => {
-			console.log(socket.connected); // true
-		});
+	const main = () => {
+		try {
+			const socket = io("https://backend.epns.io", {
+				query: {
+					did: `eip155:${botAddr}`,
+					mode: "chat",
+				},
+				autoConnect: true,
+				transports: ["websocket", "polling"],
+				reconnectionAttempts: 5,
+			});
 
-		socket.on("CHATS", async (chat) => {
-			try {
-				if (pvtKey === "") {
-					pvtKey = await getPgpKey();
-				}
-				if (Object.keys(chatUser).length == 0) {
-					chatUser = await PushAPI.user.get({
+			socket.on("connect", async () => {
+				console.log(socket.connected); // true
+			});
+
+			socket.on("CHATS", async (chat) => {
+				try {
+					if (pvtKey === "") {
+						pvtKey = await getPgpKey();
+					}
+					if (Object.keys(chatUser).length == 0) {
+						chatUser = await PushAPI.user.get({
+							account: botAddr,
+							env: "prod",
+						});
+					}
+
+					const responseReqs = await PushAPI.chat.requests({
+						pgpPrivateKey: pvtKey,
 						account: botAddr,
 						env: "prod",
 					});
+					const reqsArray = getRequestsAddrArray(responseReqs);
+					if (
+						reqsArray.length > 0 &&
+						reqsArray.includes(chat.fromDID)
+					) {
+						await approveAndSendIntroMsg(chat, pvtKey);
+					} else {
+						const response = await decryptConversation({
+							messages: [chat],
+							connectedUser: chatUser,
+							pgpPrivateKey: pvtKey,
+							env: "prod",
+						});
+						fetchResAndSend(
+							chat,
+							response[0].messageContent,
+							pvtKey
+						);
+					}
+				} catch (err) {
+					console.log(err.message);
 				}
+			});
 
-				const responseReqs = await PushAPI.chat.requests({
-					pgpPrivateKey: pvtKey,
-					account: botAddr,
-					env: "prod",
-				});
-				const reqsArray = getRequestsAddrArray(responseReqs);
-				if (reqsArray.length > 0 && reqsArray.includes(chat.fromDID)) {
-					await approveAndSendIntroMsg(chat, pvtKey);
-				} else {
-					const response = await decryptConversation({
-						messages: [chat],
-						connectedUser: chatUser,
-						pgpPrivateKey: pvtKey,
-						env: "prod",
-					});
-					fetchResAndSend(chat, response[0].messageContent, pvtKey);
-				}
-			} catch (err) {
-				console.log(err.message);
-			}
-		});
+			socket.on("disconnect", (reason) => {
+				console.log(reason);
+				// if (reason === "io server disconnect") {
+				setTimeout(() => {
+					socket.connect();
+				}, 1000);
+				// }
+			});
 
-		socket.on("disconnect", (reason) => {
-			console.log(reason);
-			// if (reason === "io server disconnect") {
-			setTimeout(() => {
-				socket.connect();
-			}, 1000);
-			// }
-		});
+			socket.on("connect_error", () => {
+				console.log("connect_error");
+				socket.io.opts.transports = ["polling", "websocket"];
+				setTimeout(() => {
+					socket.connect();
+				}, 1000);
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-		socket.on("connect_error", () => {
-			console.log("connect_error");
-			socket.io.opts.transports = ["polling", "websocket"];
-			setTimeout(() => {
-				socket.connect();
-			}, 1000);
-		});
-	} catch (err) {
-		console.log(err);
-	}
-};
+	main();
+});
 
-main();
+module.exports = app;
